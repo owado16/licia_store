807c3251d67bbb26bf51369f2a5b425b4e678bd9
owado16 <felicia.akabe1@gmail.com>
2024-04-27 02:12:57 +0100

    allowed host added on production code

--- licia_store/settings.py
+++ licia_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'grm@_$#@#i$*wm)ct235ln4jg1jxd$^tf&y2wyt5@s%(im)ckr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["licia-store.onrender.com"]
 
 
 # Application definition
@@ -135,3 +135,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
